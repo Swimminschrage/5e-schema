61e306f63fd027369513708c79e1f9d672d86017
Swimminschrage <Swimminschrage@gmail.com>
2019-12-28 00:49:02 -0800

    Updating the test script to remove modules that never got uploaded.

--- runTests.sh
+++ runTests.sh
@@ -11,6 +11,4 @@ echo ""
 echo "TESTING ACTUAL MODULES"
 echo ""
 ajv test -s module.json -r creatures.json -r creature.json -d modules/test_module.json --valid --verbose
-ajv test -s module.json -r creatures.json -r creature.json -d modules/cc_creatures.json --valid --verbose
-ajv test -s module.json -r creatures.json -r creature.json -d modules/tob_creatures.json --valid --verbose
 ajv test -s module.json -r creatures.json -r creature.json -d modules/srd_creatures.json --valid --verbose
