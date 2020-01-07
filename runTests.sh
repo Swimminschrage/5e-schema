1c283b43925aed4b152fc8092d3728f80266b565
Swimminschrage <Swimminschrage@gmail.com>
2020-01-06 18:24:44 -0800

    Adding descriptions for many of the properties in the schema. Adding a schema definition for creatures that cast spells.

--- runTests.sh
+++ runTests.sh
@@ -4,6 +4,7 @@ ajv compile -s module.json -r creatures.json -r creature.json
 
 ajv test -s module.json -r creatures.json -r creature.json -d test-files/simple-module.json --valid
 ajv test -s creature.json -d test-files/valid-single-creature.json --valid
+ajv test -s creature.json -d test-files/valid-spellcasting-creature.json --valid
 ajv test -s creature.json -d test-files/valid-with-dice.json --valid
 ajv test -s creatures.json -r creature.json -d test-files/valid-multiple-creatures.json --valid
 
