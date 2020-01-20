3cce265853d9e3abb2a85aa67d0ff75442d6ed60
Swimminschrage <Swimminschrage@gmail.com>
2020-01-20 15:55:16 -0800

    Adding support for a spells schema.

--- runTests.sh
+++ runTests.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+export PATH=$PATH:/usr/local/bin
 
-ajv compile -s module.json -r creatures.json -r creature.json
+npm install ajv
 
-ajv test -s module.json -r creatures.json -r creature.json -d test-files/simple-module.json --valid
+echo ""
+echo "COMPILING MODULE SCHEMA"
+echo ""
+ajv compile -s module.json -r creatures.json -r creature.json -r spells.json
+
+echo ""
+echo "TESTING SCHEMA AGAINST TEST DATA"
+echo ""
+ajv test -s module.json -r spells.json -r creatures.json -r creature.json -d test-files/simple-module.json --valid
 ajv test -s creature.json -d test-files/valid-single-creature.json --valid
 ajv test -s creature.json -d test-files/valid-spellcasting-creature.json --valid
+ajv test -s spells.json -d test-files/valid-spell.json --valid
 ajv test -s creature.json -d test-files/valid-with-dice.json --valid
 ajv test -s creatures.json -r creature.json -d test-files/valid-multiple-creatures.json --valid
 
 echo ""
 echo "TESTING ACTUAL MODULES"
 echo ""
-ajv test -s module.json -r creatures.json -r creature.json -d modules/test_module.json --valid --verbose
-ajv test -s module.json -r creatures.json -r creature.json -d modules/srd_creatures.json --valid --verbose
+ajv test -s module.json -r spells.json -r creatures.json -r creature.json -d modules/test_module.json --valid --verbose
+ajv test -s module.json -r spells.json -r creatures.json -r creature.json -d modules/srd_creatures.json --valid --verbose
